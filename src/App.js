0d7a522bf0f8ed10efad8d68fa42aa1277cf23f6
knorthrup83@gmail.com <knorthrup83@gmail.com>
2019-10-01 18:28:14 +0000

    End of lesson 6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <List />
-      <Note />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+
+  render () {
+    const { showNote } = this.state;
+    return (
+      <div className="App">
+        <Nav />
+        { showNote ? <Note /> : <List /> }
+      </div>
+    );
+  }
 }
 
 export default App;
