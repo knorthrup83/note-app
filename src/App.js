51334753850ba52a579a7638c76236657f94f923
knorthrup83@gmail.com <knorthrup83@gmail.com>
2019-10-01 18:17:44 +0000

    End of lesson 5

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/List';
+import Note from './components/Note';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <List />
+      <Note />
     </div>
   );
 }
