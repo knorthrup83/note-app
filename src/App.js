15f4cacb53a4e889f27dd724c23e0ad655e5b832
Kevin Northrup <knorthrup83@gmail.com>
2019-10-06 18:45:15 +0000

    End of lesson 22

--- src/App.js
+++ src/App.js
@@ -35,6 +35,10 @@ class App extends React.Component {
     .catch((err) => console.log(err.response.data) );
   }
 
+  submitNote = (data) => {
+    console.log(data);
+  }
+
   render () {
     const { showNote, notes, note } = this.state;
     return (
@@ -43,6 +47,7 @@ class App extends React.Component {
         { showNote ? 
           <Note 
             note={note}
+            submitNote={this.submitNote}
           />
           :
           <List
