6bb59fdba9ab06ef9bb53050b260fc573e9fab45
Kevin Northrup <knorthrup83@gmail.com>
2019-10-06 21:26:28 +0000

    End of lesson 34, app completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import List from './components/List';
 import Note from './components/Note';
 import axios from 'axios';
 import urlFor from './helpers/urlFor';
+import Flash from './components/Flash';
 
 class App extends React.Component {
   constructor() {
@@ -14,7 +15,8 @@ class App extends React.Component {
       showNote: false,
       notes: [],
       note: {}, 
-      newTag: false
+      newTag: false,
+      error: ''
     };
   }
 
@@ -48,7 +50,14 @@ class App extends React.Component {
   submitNote = (data, id) => {
     this.performSubmissionRequest(data, id)
     .then((res) => this.setState({ showNote: false }) )
-    .catch((err) => console.log(err.response.data) );
+    .catch((err) => {
+      const { errors } = err.response.data;
+        if (errors.content) {
+          this.setState({ error: "Missing Note Content!" });
+        } else if (errors.title) {
+          this.setState({ error: "Missing Note Title!" });
+        }
+    });
   }
 
   deleteNote = (id) => {
@@ -69,7 +78,12 @@ class App extends React.Component {
   submitTag = (data, noteId) => {
     axios.post(urlFor(`notes/${noteId}/tags`), data)
     .then((res) => this.getNote(noteId) )
-    .catch((err) => console.log(err.response.data) );
+    .catch((err) => {
+      const { errors } = err.response.data;
+      if (errors.name) {
+        this.setState({ error: "Missing Tag Name!" });
+      }
+    });
   }
 
   deleteTag = (noteId, id) => {
@@ -78,12 +92,17 @@ class App extends React.Component {
     .catch((err) => console.log(err.response.data) );
   }
 
+  resetError = () => {
+    this.setState({ error: '' });
+  }
+
   render () {
-    const { showNote, notes, note, newTag } = this.state;
+    const { showNote, notes, note, newTag, error } = this.state;
 
     return (
       <div className="App">
         <Nav toggleNote={this.toggleNote}  showNote={showNote} />
+        {error && <Flash error={error} resetError={this.resetError} />}
         { showNote ? 
           <Note 
             note={note}
