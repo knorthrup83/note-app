3b78a6719236584ffd886f143ece524aee7544c7
Kevin Northrup <knorthrup83@gmail.com>
2019-10-06 17:48:12 +0000

    End of lesson 16

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NoteCard from './NoteCard';
 
 class List extends React.Component {
 	componentWillMount() {
@@ -6,9 +7,21 @@ class List extends React.Component {
   }
 
   render() {
+    const { notes } = this.props;
+
+    const cards = notes.map((note, index) => {
+      return (
+        <NoteCard
+          key={index}
+          index={index}
+          note={note}
+        />
+      );
+    });
+
 		return (
 			<div className="list-container">
-				List Component
+				{cards};
 			</div>
 		);
 	}
